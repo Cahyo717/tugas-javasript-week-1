26b64423bf878af42188d9cf01fa145df5710ea2
Cahyo Novianto <cahyonovianto97@gmail.com>
2022-11-11 13:35:56 +0700

    menambahkan komentar di file tugas1.js

--- tugas1.js
+++ tugas1.js
@@ -1,13 +1,15 @@
 //membuat penjumlahan array
 
 function arraySum (array){
-    const arrayResult = [];
+    const arrayResult = []; //array kosong untuk menampung hasil penjumlahan
     let sum = 0
 
+    //menjumlahkan keseluruhan array
     for(let i = 0; i < array.length; i++){
        sum += array[i];
     }
 
+    //mengurangi jumlah tersebu dengan nilai array dari index ke i, kemudian menambahkan hasilnya ke arrayResult = []
     for(let n = 0; n < array.length; n++){
         let currentSum = sum - array[n];
         arrayResult.push(currentSum);
@@ -15,6 +17,7 @@ function arraySum (array){
 
     }
 
+    //mencari nilai maksimal dan minimal dari arraySum
     let min = arrayResult[0],max = arrayResult[0]
     for(let a = 0; a < arrayResult.length; a++){
         if (arrayResult[a] < min){
